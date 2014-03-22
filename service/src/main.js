f68d8d338ea4c020d468cb3e1b4029d305a87987
Niko Köbler <niko@n-k.de>
2014-03-22 22:03:10 +0100

    set up first version of twitterwall with persistence in filedataprovider and sending tweets to client with push notification (server sent events)

--- service/src/main.js
+++ service/src/main.js
@@ -1,28 +1,57 @@
-var avatar = require("org/glassfish/avatar");
-
+// references to Java objects
 var TwitterStreamFactory = Java.type("twitter4j.TwitterStreamFactory");
-var FilterQuery          = Java.type("twitter4j.FilterQuery");
-var StatusListener       = Java.type("twitter4j.StatusListener");
+var FilterQuery = Java.type("twitter4j.FilterQuery");
+var StatusListener = Java.type("twitter4j.StatusListener");
 
-var jms = new avatar.JMS({connectionsFactoryName: "jms/__defaultConnectionFactory", destinationName: "jms/avatarQ"});
-jms.send("Test message");
+// requires
+var avatar = require("org/glassfish/avatar");
+var conf = require("twitter-config").conf();
+var _ = require("underscore-min");
 
-var listener = new StatusListener() {
+// set up data provider
+var dataProvider = new avatar.FileDataProvider({filename: "/tmp/tweets.txt", key: "id"});
+dataProvider.delCollection();
+
+// set up Twitter listener
+var listener = new StatusListener({
     onStatus: function(status) {
-        avatar.log("@" + status.user.screenName + "\t" + status.text);
-        jms.send(status);
+		// convert the status Java object to a JSON object
+		var tweet = {id: status.id,
+			createdAt: (status.createdAt + ""),
+			screenName: status.user.screenName,
+			text: status.text};
+        dataProvider.create(tweet);
+    },
+    onException: function(err) {
+        avatar.log("t4jException occurred: " + err);
     }
-}
+});
 
-var twitter = new TwitterStreamFactory().instance;
+// get Twitter instance
+var twitter = new TwitterStreamFactory(conf.build()).instance;
 twitter.addListener(listener);
-twitter.filter(new FilterQuery(0, [], ['Justin Bieber']));
 
-avatar.registerPushService({
-    url: "push/tweeds",
-    jms: {
-        connectionsFactoryName: "",
-        destinationName: "",
-        jms: this.jms
+// register pushService
+var tweets;
+avatar.registerPushService({url: "push/tweets"},
+    function() {
+        this.onOpen = function(context) {
+			avatar.log("onOpen called");
+			context.setTimeout(1000);
+			twitter.filter(new FilterQuery(0, [], ['Starbucks']));
+		},
+		this.onTimeout = function(context) {
+			dataProvider.getCollection()
+				.then(function(result){
+					var data = result.data;
+					tweets = {items: _.last(data, 25).reverse()};
+				});
+            context.setTimeout(2000);
+			return context.sendMessage(tweets);
+        },
+		this.onClose = function(context) {
+			avatar.log("onClose called");
+			twitter.cleanUp();
+		};
     }
-}, function(){});
+);
