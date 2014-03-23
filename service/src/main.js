43539b5d072a3854f0341b15d6b8fd9297ab3afa
Niko Köbler <niko@n-k.de>
2014-03-23 11:45:47 +0100

    jslinting and jshinting

--- service/src/main.js
+++ service/src/main.js
@@ -1,12 +1,14 @@
-// references to Java objects
-var TwitterStreamFactory = Java.type("twitter4j.TwitterStreamFactory");
-var FilterQuery = Java.type("twitter4j.FilterQuery");
-var StatusListener = Java.type("twitter4j.StatusListener");
+"use strinct";
 
 // requires
 var avatar = require("org/glassfish/avatar");
 var conf = require("twitter-config").conf();
 var _ = require("underscore-min");
+
+// references to Java objects
+var TwitterStreamFactory = Java.type("twitter4j.TwitterStreamFactory");
+var FilterQuery = Java.type("twitter4j.FilterQuery");
+var StatusListener = Java.type("twitter4j.StatusListener");
 
 // set up data provider
 var dataProvider = new avatar.FileDataProvider({filename: "/tmp/tweets.txt", key: "id"});
@@ -14,15 +16,15 @@ dataProvider.delCollection();
 
 // set up Twitter listener
 var listener = new StatusListener({
-    onStatus: function(status) {
-		// convert the status Java object to a JSON object
-		var tweet = {id: status.id,
-			createdAt: (status.createdAt + ""),
-			screenName: status.user.screenName,
-			text: status.text};
+    onStatus: function (status) {
+        // convert the status Java object to a JSON object
+        var tweet = {id: status.id,
+            createdAt: (status.createdAt + ''),
+            screenName: status.user.screenName,
+            text: status.text};
         dataProvider.create(tweet);
     },
-    onException: function(err) {
+    onException: function (err) {
         avatar.log("t4jException occurred: " + err);
     }
 });
@@ -34,60 +36,62 @@ twitter.addListener(listener);
 // init local variables
 var query;
 var tweets;
-initVars = function() {
-	query = "";
-	tweets = {items: []};
-}
+var initVars = function () {
+    query = "";
+    tweets = {items: []};
+};
 
-stopTwitter = function() {
-	avatar.log("stop receiving Twitter updates");
-	twitter.cleanUp();
-	initVars();
-}
+var stopTwitter = function () {
+    avatar.log("stop receiving Twitter updates");
+    twitter.cleanUp();
+    initVars();
+};
 
 // register restService for receiving search queries
-avatar.registerRestService({ url: "rest/query" },
-    function() {
-        this.onPut = function(request, response) {
+avatar.registerRestService(
+    { url: "rest/query" },
+    function () {
+        this.onPut = function (request, response) {
             query = request.data.query;
-			avatar.log("onPut called: " + query);
-			if (query !== "") {
-				twitter.filter(new FilterQuery(0, [], [query]));
-			}
+            avatar.log("onPut called: " + query);
+            if (query !== "") {
+                twitter.filter(new FilterQuery(0, [], [query]));
+            }
             response.send(null);
         };
-		this.onDelete = function(request, response) {
-			avatar.log("onDelete called");
-			stopTwitter();
-			response.send(null);
-		};
-		this.onGet = function(req, res) {
-			res.send(query || '');
-		}
+        this.onDelete = function (request, response) {
+            avatar.log("onDelete called");
+            stopTwitter();
+            response.send(null);
+        };
+        this.onGet = function (req, res) {
+            res.send(query || '');
+        };
     }
 );
 
 // register pushService
-avatar.registerPushService({url: "push/tweets"},
-    function() {
-        this.onOpen = function(context) {
-			avatar.log("onOpen called - init push service");
-			initVars();
-			context.setTimeout(1);
-		},
-		this.onTimeout = function(context) {
-			// get last 25 Twitter messages from data provider
-			dataProvider.getCollection()
-				.then(function(result){
-					tweets = {items: _.last(result.data, 25).reverse()};
-				});
+avatar.registerPushService(
+    {url: "push/tweets"},
+    function () {
+        this.onOpen = function (context) {
+            avatar.log("onOpen called - init push service");
+            initVars();
+            context.setTimeout(1);
+        };
+        this.onTimeout = function (context) {
+            // get last 25 Twitter messages from data provider
+            dataProvider.getCollection()
+                .then(function (result) {
+                    tweets = {items: _.last(result.data, 25).reverse()};
+                });
             context.setTimeout(2000);
-			// send tweets to client(s)
-			return context.sendMessage(tweets);
-        },
-		this.onClose = function(context) {
-			avatar.log("onClose called");
-			stopTwitter();
-		};
+            // send tweets to client(s)
+            return context.sendMessage(tweets);
+        };
+        this.onClose = function (context) {
+            avatar.log("onClose called");
+            stopTwitter();
+        };
     }
 );
